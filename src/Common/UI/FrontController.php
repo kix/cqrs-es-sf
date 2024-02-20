d4f568a59a0c78ded1da168dae6e1cf56f208b60
Stepan Anchugov <kixxx1@gmail.comm>
2024-02-20 18:17:58 +0100

    Init a project with a simple front controller

---
+++ src/Common/UI/FrontController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\UI;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Front controller is responsible for receiving a request, matching it against a command/query dictionary,
+ * dispatching it and returning a response.
+ *
+ * Exceptions should be handled by regular Symfony handlers.
+ *
+ * @package Facade
+ */
+final class FrontController
+{
+    public function __invoke(Request $request): Response
+    {
+
+    }
+}
